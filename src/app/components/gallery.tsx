e8590501e5956d9b1b4496d38807ede6d01831ea
Angel Capa <ricardocmdn@gmail.com>
2025-06-04 15:35:20 -0500

    add swiper titles

--- src/app/components/gallery.tsx
+++ src/app/components/gallery.tsx
@@ -2,10 +2,11 @@
 
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Keyboard } from "swiper/modules";
-import "swiper/css";
+import { Keyboard, Mousewheel } from "swiper/modules";
+import "swiper/swiper-bundle.css";
 
 import { PublicationsWp } from "../_interfaces/wordpress-components";
+import { getProxyImageUrl } from "@/utils/image_proxy";
 
 interface GalleryProps {
   publication: PublicationsWp[];
@@ -15,6 +16,18 @@ const GalleryProjects: React.FC<GalleryProps> = ({ publication }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const swiperRef = useRef<any>(null);
 
+  const handleMouseEnter = () => {
+    if (swiperRef.current?.mousewheel) {
+      swiperRef.current.mousewheel.enable();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (swiperRef.current?.mousewheel) {
+      swiperRef.current.mousewheel.disable();
+    }
+  };
+
   return (
     <div className="relative w-screen h-auto">
       <button
@@ -23,43 +36,62 @@ const GalleryProjects: React.FC<GalleryProps> = ({ publication }) => {
       >
         <img src="/images/left.svg" alt="Previous" />
       </button>
-      <Swiper
-        modules={[Keyboard]}
-        slidesPerView={6.1}
-        centeredSlides={true}
-        spaceBetween={5}
-        loop={true}
-        keyboard={{ enabled: true }}
-        onSlideChange={(swiper) => setSelectedIndex(swiper.realIndex)}
-        onBeforeInit={(swiper) => {
-          swiperRef.current = swiper;
-        }}
-        initialSlide={selectedIndex}
-        style={{
-          width: "100vw",
-          boxSizing: "border-box",
-        }}
-      >
-        {publication.map((project, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={project.image.url}
-              alt={project.image.alt || `Image ${index + 1}`}
-              onClick={() => {
-                swiperRef.current?.slideToLoop(index);
-                setSelectedIndex(index);
-              }}
-              className={`cursor-pointer transition-transform duration-300 mx-auto
-    ${
-      index === selectedIndex ? "h-[370px] opacity-100" : "h-[290px] opacity-30"
-    }
-  `}
-              style={{ width: "100%" }}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
 
+      <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <Swiper
+          modules={[Keyboard, Mousewheel]}
+          slidesPerView={6.1}
+          centeredSlides={true}
+          spaceBetween={5}
+          loop={true}
+          keyboard={{ enabled: true }}
+          mousewheel={{
+            forceToAxis: false,
+            sensitivity: 1,
+          }}
+          onSlideChange={(swiper) => setSelectedIndex(swiper.realIndex)}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+            swiper.mousewheel.disable();
+          }}
+          initialSlide={selectedIndex}
+          style={{
+            width: "100vw",
+            boxSizing: "border-box",
+          }}
+        >
+          {publication.map((project, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={getProxyImageUrl(project.image.url)}
+                alt={project.image.alt || `Image ${index + 1}`}
+                onClick={() => {
+                  swiperRef.current?.slideToLoop(index);
+                  setSelectedIndex(index);
+                }}
+                className={`cursor-pointer transition-transform duration-300
+                  ${
+                    index === selectedIndex
+                      ? "h-[370px] opacity-100"
+                      : "h-[290px] opacity-30"
+                  }
+                `}
+                style={{ width: "100%" }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      {publication[selectedIndex] && (
+        <div className="mt-[16px] text-center">
+          <h2 className="uppercase text-[16px] leading-[22px]">
+            {publication[selectedIndex].title}
+          </h2>
+          <p className="uppercase text-[16px] leading-[22px]">
+            {publication[selectedIndex].sub_title}
+          </p>
+        </div>
+      )}
       <button
         onClick={() => swiperRef.current?.slideNext()}
         className="absolute right-[40px] top-[380px] -translate-y-1/2 z-10"
