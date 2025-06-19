e3a25c6de6bda4ea53862f928e11bf9040492ef5
Angel Capa <ricardocmdn@gmail.com>
2025-06-18 19:29:44 -0500

    add changes responsive

--- src/app/components/cover-pages.tsx
+++ src/app/components/cover-pages.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
 import { getProxyImageUrl } from "@/utils/image_proxy";
 import { MediaFileWp } from "../_interfaces/wordpress-components";
+import { useEffect, useState } from "react";
 
 interface Props {
   children?: React.ReactElement;
@@ -11,50 +12,53 @@ interface Props {
 }
 
 export function Cover(props: Props) {
-  const {
-    children,
-    className,
-    media,
-    img
-  } = props;
+  const { children, className, media, img } = props;
+
+  const [height, setHeight] = useState("100vh");
+
+  useEffect(() => {
+    const updateHeight = () => {
+      setHeight(`${window.innerHeight - 50}px`);
+    };
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+    window.addEventListener("scroll", updateHeight);
+
+    return () => {
+      window.removeEventListener("resize", updateHeight);
+      window.removeEventListener("scroll", updateHeight);
+    };
+  }, []);
 
   return (
     <div className={`cover-video-container`}>
       {img && (
         <img
-        src={img}
-        className={`${className} w-full h-[calc(100dvh-50px)] object-cover`}
-      />
+          src={img}
+          className={`${className} w-full object-cover`}
+          style={{ height }}
+        />
       )}
-      {media?.type === 'video' && (
+      {media?.type === "video" && (
         <video
-          className={`${className} w-full h-[calc(100dvh-50px)] object-cover`}
-          autoPlay={true}
-          loop={true}
+          className={`${className} w-full object-cover`}
+          autoPlay
+          loop
           muted
           playsInline
           preload="auto"
+          style={{ height }}
         >
           <source src={media.url} type="video/mp4" />
         </video>
       )}
-      {media?.type === 'image' && (
+      {media?.type === "image" && (
         <img
           src={getProxyImageUrl(media.url)}
-          className={`${className} w-full h-[calc(100dvh-50px)] object-cover`}
+          className={`${className} w-full object-cover`}
+          style={{ height }}
         />
       )}
-      {/* <div
-        className={`absolute h-[calc(100dvh-50px)] inset-0 ${className}`}
-        style={{
-          backgroundImage:
-            'linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.25))',
-          zIndex: 1,
-        }}
-      /> */}
-      {/* <div className="absolute inset-0 flex items-center justify-center z-10">
-        {children}
-      </div> */}
     </div>
   );
 }
