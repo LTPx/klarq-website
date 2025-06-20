b7fddda5a0876fb546e1ea0dba26d88299db025f
Angel Capa <ricardocmdn@gmail.com>
2025-06-19 23:30:42 -0500

    add changes

--- src/app/components/decor-projects.tsx
+++ src/app/components/decor-projects.tsx
@@ -40,17 +40,28 @@ function DecorProjects(props: Props) {
       <div className="grid gap-[22px] lg:gap-[0px] grid-cols-1 lg:grid-cols-2 pt-[10px] lg:pt-[25px]">
         <div className="flex pr-[15px] lg:pr-[0px] flex-col lg:justify-start justify-between flex-row">
           <div className="flex items-center justify-between w-full">
-            <h2 className="text-[16px] lg:text-[18px] leading-[22px]">{title} <br className="lg:hidden"/> <span className="lg:hidden">{date}</span></h2>
+            <h2 className="text-[16px] lg:text-[18px] leading-[22px]">
+              {title} <br className="lg:hidden" />{" "}
+              <span className="lg:hidden">{date}</span>
+            </h2>
             <button
               onClick={toggleExpanded}
               className="lg:hidden text-[24px] font-bold"
               aria-label="Toggle Description"
             >
-              {isExpanded ? "−" : "+"}
+              <img
+                src="/images/more.svg"
+                className={`transition-transform duration-300 ${
+                  isExpanded
+                    ? "rotate-45 h-[15px] w-[15px]"
+                    : "rotate-0 h-[15px] w-[15px]"
+                }`}
+              />
             </button>
           </div>
-          <span className="hidden lg:block text-[16px] lg:text-[18px] leading-[22px]">{date}</span>
-          
+          <span className="hidden lg:block text-[16px] lg:text-[18px] leading-[22px]">
+            {date}
+          </span>
         </div>
         <div
           className="hidden lg:block designer-description pr-[15px] lg:pr-[50px]"
