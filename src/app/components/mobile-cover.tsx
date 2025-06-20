3740726eadb4b932f3fc51c4347cab9e182f5e8f
Angel Capa <ricardocmdn@gmail.com>
2025-06-19 19:08:51 -0500

    add changes scroll page

--- src/app/components/mobile-cover.tsx
+++ src/app/components/mobile-cover.tsx
@@ -31,6 +31,10 @@ export default function MobileCover({
   }, [progress]);
 
   useEffect(() => {
+    setLocked(false);
+  }, []);
+
+  useEffect(() => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   }, []);
