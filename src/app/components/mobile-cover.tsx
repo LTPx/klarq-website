fc36919b1e10bdbe47fddfecd8e0a290654b106a
Angel Capa <ricardocmdn@gmail.com>
2025-06-19 21:55:20 -0500

    add changes

--- src/app/components/mobile-cover.tsx
+++ src/app/components/mobile-cover.tsx
@@ -34,20 +34,9 @@ export default function MobileCover({
     setLocked(false);
   }, []);
 
-  // useEffect(() => {
-  //   const vh = window.innerHeight * 0.01;
-  //   document.documentElement.style.setProperty("--vh", `${vh}px`);
-  // }, []);
-
   useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-  
-    setVh();
-    window.addEventListener("resize", setVh);
-    return () => window.removeEventListener("resize", setVh);
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
   }, []);
 
   return (
