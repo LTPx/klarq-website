0c02affc85558a38f59c4ae28dc23a04c574d3f7
Angel Capa <ricardocmdn@gmail.com>
2025-09-11 13:05:33 -0500

    add changes

--- src/app/components/architecture-wrapper.tsx
+++ src/app/components/architecture-wrapper.tsx
@@ -2,11 +2,10 @@
 
 import { useEffect, useState } from "react";
 import { InformationWp } from "../_interfaces/wordpress-components";
-
-import ArchitectureDesktop from "./architecture-desktop";
-import ArchitecturePageMobile from "./architecture-mobile";
 import { WordPressFrontendPage } from "../_interfaces/wordpress-page";
+import ArchitectureDesktop from "./architecture-desktop";
 import ArchitectureTablet from "./architecture-tablet";
+import ArchitecturePageMobile from "./architecture-mobile";
 
 interface Props {
   projects: {
@@ -23,20 +22,29 @@ function ArchitectureWrapper({ projects, information }: Props) {
   const [device, setDevice] = useState<DeviceType>("desktop");
 
   useEffect(() => {
-    function handleResize() {
-      const width = window.innerWidth;
-      if (width <= 900) {
-        setDevice("mobile");
-      } else if (width <= 1200) {
-        setDevice("tablet");
-      } else {
-        setDevice("desktop");
-      }
-    }
+    const mediaQueries = {
+      mobile: window.matchMedia("(max-width: 767px)"),
+      tablet: window.matchMedia("(min-width: 768px) and (max-width: 1024px)"),
+      desktop: window.matchMedia("(min-width: 1025px)")
+    };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    const handleDeviceChange = () => {
+      if (mediaQueries.mobile.matches) setDevice("mobile");
+      else if (mediaQueries.tablet.matches) setDevice("tablet");
+      else if (mediaQueries.desktop.matches) setDevice("desktop");
+    };
+
+    handleDeviceChange();
+
+    Object.values(mediaQueries).forEach((mq) =>
+      mq.addEventListener("change", handleDeviceChange)
+    );
+
+    return () => {
+      Object.values(mediaQueries).forEach((mq) =>
+        mq.removeEventListener("change", handleDeviceChange)
+      );
+    };
   }, []);
 
   return (
